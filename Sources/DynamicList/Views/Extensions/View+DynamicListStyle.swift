fb2ce4f9d9c5cd6aea09acc377ed89b493b16185
Jesús Alfredo Hernández Alarcón <alfredo.h.alarcon@icloud.com>
2023-07-13 19:31:34 -0600

    fix: Fix unavailable list styles for macos

--- Sources/DynamicList/Views/Extensions/View+DynamicListStyle.swift
+++ Sources/DynamicList/Views/Extensions/View+DynamicListStyle.swift
@@ -10,14 +10,20 @@ extension View {
         switch type {
         case .plain:
             listStyle(.plain)
+        #if os(iOS)
         case .grouped:
             listStyle(.grouped)
+        #endif
         case .inset:
             listStyle(.inset)
+        #if os(iOS)
         case .insetGrouped:
             listStyle(.insetGrouped)
+        #endif
         case .sidebar:
             listStyle(.sidebar)
+        case .insetGrouped, .grouped:
+            listStyle(.automatic)
         }
     }
 }
